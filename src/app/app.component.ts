377c2cc57805c36b981551f93fc841d0590c0fdc
dingruiqi <dingruiqi1981@163.com>
2019-07-24 17:09:08 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  //selector: 'app-root',
+  selector: 'app',
+  //templateUrl: './app.component.html',
+  //template: '<div class="bg-success p-a-1 h1 text-center">This is SportsStore</div>',
+  template: '<store></store>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SportsStore';
+  //title = 'SportsStore';
 }
