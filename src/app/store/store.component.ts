af5dd7d387e1cdbf734e9d6d7158fccef4f79937
dingruiqi <dingruiqi1981@163.com>
2019-07-25 08:57:09 +0800

    测试

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -40,5 +40,5 @@ export class StoreComponent {
 
     get pageNumbers(): number[] {
         return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)).fill(0).map((x, i) => i + 1);
-    } 
+    }
 }
